d2bd1c56d236b4945e064bfbf4ce0500b9fb6b7a
Brian <fisetbrian@gmail.com>
2023-08-30 14:34:06 -0400

    Add game logic console shows who won

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
-const Player = (name, marker) => {
+const Player = (name, marker, win) => {
     return{name, marker};
 };
 
-const player1 = Player('Player 1', 'X');
-const player2 = Player('Player 2', '0');
+const player1 = Player('Player 1', 'X',false);
+const player2 = Player('Player 2', '0',false);
 
 const gameBoard = (() => {
     let turn = player1
     const cells = document.querySelectorAll('.cells');
     cells.forEach(cell => cell.addEventListener('click', (e) => {
-        if(e.target.innerText !== ''){
+        if(e.target.innerText !== '' || player1.win || player2.win){
             return
         } if(turn === player1) {
             e.target.innerText = `${player1.marker}`;
@@ -19,5 +19,33 @@ const gameBoard = (() => {
             turn = player1
         }
     }))
-    
 })();
+
+const gameLogic = (() => {
+    function allElementsSame(array, x){
+        return array.every(element => element === x)
+    }
+    function win() {
+        if(player1.win || player2.win) return
+        const cells = document.querySelectorAll('.cells');
+        const winConditions = [
+            [cells[0].innerText,cells[1].innerText,cells[2].innerText],
+            [cells[3].innerText,cells[4].innerText,cells[5].innerText],
+            [cells[6].innerText,cells[7].innerText,cells[8].innerText],
+            [cells[0].innerText,cells[3].innerText,cells[6].innerText],
+            [cells[1].innerText,cells[4].innerText,cells[7].innerText],
+            [cells[2].innerText,cells[5].innerText,cells[8].innerText],
+            [cells[0].innerText,cells[4].innerText,cells[8].innerText],
+            [cells[2].innerText,cells[4].innerText,cells[6].innerText]
+        ]
+        for(let i = 0; i < winConditions.length; i++) {
+            if(allElementsSame(winConditions[i], player1.marker)){
+                player1.win = true
+                console.log(player1.name)
+            } else if(allElementsSame(winConditions[i], player2.marker)){
+                player2.win = true
+                console.log(player2.name)
+        }}
+    }
+    document.querySelector('#game-container').addEventListener('click', win)
+})()
