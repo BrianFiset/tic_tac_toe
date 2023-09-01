ba4db47446936568d8440871080b37d104f60ccf
Brian <fisetbrian@gmail.com>
2023-09-01 10:16:12 -0400

    Fix restart glitch wrong marker and finishing styles

--- app.js
+++ app.js
@@ -7,41 +7,59 @@ const player2 = Player('Player 2', 'O', 'easy');
 
 const gameBoard = (() => {
     let playerTurn = player1;
+
+    const changeTurn = () => {
+        return playerTurn = (playerTurn === player1) ? player2 : player1;
+    }
+
+    const opponent = document.querySelector('#opponent');
+
+    opponent.addEventListener('change',() => {
+        if(opponent.value === 'human') {
+            player2.ai = undefined;
+        } else {
+            player2.ai = 'easy';
+        };
+        gameLogic.startGame();
+    });
+
     const cells = document.querySelectorAll('.cells');
-    cells.forEach(cell => cell.addEventListener('click', (e) => {
+
+    const addMarker =  (e) => {
         if(e.target.innerText !== '' || player1.win || player2.win){
             return
         } if(playerTurn === player1) {
             e.target.innerText = `${player1.marker}`;
-            playerTurn = player2;
+            changeTurn();
             gameLogic.win();
             if(player1.win || player2.win) return
             if(player2.ai === 'easy'){
                 gameAi.easyMode();
-                playerTurn = player1;
+                changeTurn();
             };
         } else if(playerTurn === player2){
-            console.log(playerTurn)
             e.target.innerText = `${player2.marker}`;
-            playerTurn = player1;
+            changeTurn();
+            gameLogic.win();
         };
-    }));
-    return{cells, playerTurn}
+    };
+
+    cells.forEach(cell => cell.addEventListener('click', addMarker));
+    return{cells, changeTurn, playerTurn}
 })();
 
 const gameAi = (() => {
-    const cellsArray = Array.prototype.slice.call(gameBoard.cells);
     const randomNumber = (x) => {
         return Math.floor(Math.random() * x);
     }
     const easyMode = () => {
-        const emptyCells = cellsArray.filter(cell => cell.innerText === '');
+        emptyCells = gameLogic.emptyCells();
         if(emptyCells.length === 0) return
         emptyCells[randomNumber(emptyCells.length)].innerText = player2.marker;
         gameLogic.win();
     }
 
-    return{easyMode}
+    return{easyMode};
 })()
 
 
@@ -59,28 +77,27 @@ const gameLogic = (() => {
         return Math.floor(Math.random() * number)
     };
 
+    const emptyCells = ()=> {
+        return cellsArray().filter(cell => cell.innerText === '');
+    };
+
     const cellsArray = () => {
-        const cellArray = [
-            cells[0].innerText,cells[1].innerText,cells[2].innerText,
-            cells[3].innerText,cells[4].innerText,cells[5].innerText,
-            cells[6].innerText,cells[7].innerText,cells[8].innerText
-        ];
-        return cellArray
-    }
+        return Array.prototype.slice.call(gameBoard.cells);
+    };
 
     const tie = () => {
-        return cellsArray().every(element => (element === player1.marker || element === player2.marker));
+       return emptyCells().length === 0;
     };
 
     const gameOver = () =>{
         gameOverScreen.classList.add('flex');
         cells.forEach(cell => cell.classList.add('blur'));
         if(player1.win) {
-            winner.textContent = `${player1.name} has Won the Game`
+            winner.textContent = `${player1.name} has Won the Game`;
         }else if(player2.win){
-            winner.textContent = `${player2.name} has Won the Game`
+            winner.textContent = `${player2.name} has Won the Game`;
         } else {
-            winner.textContent = `Tied`
+            winner.textContent = `Tied`;
         }
     };
     
@@ -90,9 +107,12 @@ const gameLogic = (() => {
         cells.forEach(cell => cell.innerText = '');
         player1.win = false;
         player2.win = false;
+        if(gameBoard.changeTurn() === player2) {
+            gameBoard.changeTurn();
+        }
     };
 
-    retryBtn.addEventListener('click', startGame)
+    retryBtn.addEventListener('click', startGame);
 
     const win = () => {
         if(player1.win || player2.win) return
@@ -118,5 +138,5 @@ const gameLogic = (() => {
             };
         };
     };
-    return {cellsArray,allElementsSame, randomNumber, win}
+    return {cellsArray,allElementsSame, randomNumber, win, startGame, emptyCells}
 })();
